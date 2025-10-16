6c4f40216ac31bb06416b74a7f5a945df7b6ab68
nordluma <maxnordlund.95@hotmail.com>
2025-10-16 20:39:07 +0300

    feat(internal/ui): add model for showing search result (hard coded)

---
+++ internal/ui/anime_details.go
@@ -0,0 +1,38 @@
+package ui
+
+import (
+	"fmt"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/nordluma/yaapr/internal/models"
+)
+
+type AnimeDetailsModel struct {
+	anime models.Anime
+}
+
+func NewAnimeDetails(a models.Anime) AnimeDetailsModel {
+	return AnimeDetailsModel{anime: a}
+}
+
+func (m AnimeDetailsModel) Init() tea.Cmd { return nil }
+
+func (m AnimeDetailsModel) Update(msg tea.Msg) (Screen, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "esc", "q":
+			return m, func() tea.Msg { return PopScreenMsg{} }
+		}
+	}
+
+	return m, nil
+}
+
+func (m AnimeDetailsModel) View() string {
+	return fmt.Sprintf(
+		"Anime Details:\n\nTitle: %s\nID: %s\n\n[esc] Back",
+		m.anime.Title.English,
+		m.anime.ID,
+	)
+}
--- internal/ui/search.go
+++ internal/ui/search.go
@@ -1,6 +1,9 @@
 package ui
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/nordluma/yaapr/internal/models"
+)
 
 type SearchModel struct{}
 
@@ -13,6 +16,18 @@ func (m SearchModel) Update(msg tea.Msg) (Screen, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
+		case "enter":
+			selected := models.Anime{
+				ID: "123",
+				Title: models.AnimeTitle{
+					English: "Berserk",
+					Romaji:  "Berserk",
+				},
+			}
+
+			return m, func() tea.Msg {
+				return PushScreenMsg{Screen: NewAnimeDetails(selected)}
+			}
 		case "esc", "q":
 			return m, func() tea.Msg { return PopScreenMsg{} }
 		}
