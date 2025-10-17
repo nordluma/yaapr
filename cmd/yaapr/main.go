b13d5ad5c6a340f401c197590946137d754db2cd
nordluma <maxnordlund.95@hotmail.com>
2025-10-18 01:06:43 +0300

    feat(cmd/yaapr): setup logging

--- cmd/yaapr/main.go
+++ cmd/yaapr/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/nordluma/yaapr/internal/ui"
 )
 
 func main() {
+	f, err := tea.LogToFile("debug.log", "debug")
+	if err != nil {
+		fmt.Println("fatal", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
 	p := tea.NewProgram(ui.NewApp())
 	if _, err := p.Run(); err != nil {
 		panic(err)
