95bb1ee969e1ed0a98c25e92934fec0cc861dad0
nordluma <maxnordlund.95@hotmail.com>
2025-10-18 20:59:57 +0300

    feat(internal/allanime): define client for allanime

---
+++ internal/allanime/client.go
@@ -0,0 +1,67 @@
+package allanime
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+const (
+	allanimeUrl       = "https://api.allanime.day/api"
+	allanimeUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/121.0"
+	allanimeReferer   = "https://allanime.to"
+)
+
+type AllanimeClient struct {
+	client *http.Client
+}
+
+func NewAllanimeClient() *AllanimeClient {
+	return &AllanimeClient{
+		client: &http.Client{},
+	}
+}
+
+func (c AllanimeClient) do(
+	ctx context.Context,
+	query string,
+	variables, v any,
+) error {
+	data, err := json.Marshal(variables)
+	if err != nil {
+		return err
+	}
+
+	urlParams := url.Values{}
+	urlParams.Set("query", query)
+	urlParams.Set("variables", string(data))
+
+	req, err := http.NewRequestWithContext(
+		ctx,
+		"GET",
+		fmt.Sprintf("%s?%s", allanimeUrl, urlParams.Encode()),
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("User-Agent", allanimeUserAgent)
+	req.Header.Set("Referer", allanimeReferer)
+
+	res, err := c.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(res.Body)
+		return fmt.Errorf("non-OK response (%d): %s", res.StatusCode, body)
+	}
+
+	return json.NewDecoder(res.Body).Decode(&v)
+}
